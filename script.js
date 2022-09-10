3bc1b7602fd305ff53a7cb2d53aeba76edaad353
Zac <zacheryngky@gmail.com>
2022-09-10 23:50:23 +0800

    Add rainbow color functionality

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const gridContainer = document.querySelector('.grid-container');
+const blackBtn = document.querySelector("#black");
+const rainbowBtn = document.querySelector("#rainbow");
+const DEFAULT_COLOR = "rgb(0,0,0)";
+
 // takes no. of grid cells per side, n
 // Creates a n x n sized grid
 function createGrid(n) {
@@ -18,9 +22,43 @@ function createGrid(n) {
 // Set the hover effect on grid cells
 function setHover(HTMLCollection) {
     for (let i = 0; i < HTMLCollection.length; i++) {
-        HTMLCollection[i].addEventListener("mouseover", (e) => e.target.style.backgroundColor = "black");
+        HTMLCollection[i].addEventListener("mouseover", (e) => changeBackgroundColor(e));
+    }   
+}
+
+function changeBackgroundColor(e) {
+    if (blackBtn.classList.contains("active")) {
+        e.target.style.setProperty("background-color", `${DEFAULT_COLOR}`);
+    } else if (rainbowBtn.classList.contains("active")) {
+        e.target.style.setProperty("background-color", `${getRandomRGB()}`);
     }
 }
+
+function getRandomRGB() {
+    let randomR = Math.floor(Math.random() * 255);
+    let randomG = Math.floor(Math.random() * 255);
+    let randomB = Math.floor(Math.random() * 255);
+    return `rgb(${randomR},${randomG},${randomB})`;
+}
+
+blackBtn.addEventListener("click", (e) => {
+    if (blackBtn.classList.contains("active")) {
+        return;
+    } else {
+        blackBtn.classList.add("active");
+        rainbowBtn.classList.remove("active");
+    }
+});
+
+rainbowBtn.addEventListener("click", (e) => {
+    if (rainbowBtn.classList.contains("active")) {
+        return;
+    } else {
+        rainbowBtn.classList.add("active");
+        blackBtn.classList.remove("active");
+        currentColor = "#ff0000";
+    }
+});
 
 function updateSliderText(cellsEachSide) {
     const sliderText = document.getElementById("slider-text");
